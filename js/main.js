b4e6529aadb6df12947be2fb3882af2d47867be4
Thomas Hynard <tomhynard@gmail.com>
2018-02-16 10:36:04 +1100

    Derp

--- js/main.js
+++ js/main.js
@@ -1,29 +1,29 @@
-var domReady = function(callback) {
+(function(){
+  domReady(init);
+
+  function domReady(callback) {
     var readyStates = ['interactive', 'complete'];
     readyStates.indexOf(document.readyState > -1) ? callback() : document.addEventListener('DOMContentLoaded', callback);
-};
+  }
 
-var init = function() {
+  function init() {
     var logo = document.getElementById('logo');
-    var colorLabel = document.getElementById('color');
     var wordsLabel = document.getElementById('words');
 
     var toggleTheme = function() {
-        var color = getRandomColor();
-        logo.style.background = color;
-        colorLabel.innerHTML = color;
-        wordsLabel.innerHTML = words(5).join(' ');
+      var randomColor = getRandomColor();
+      logo.style.background = randomColor;
+      wordsLabel.innerHTML = words(6).join(' ');
     };
 
     var getRandomColor = function() {
-        return '#' + Math.floor(Math.random() * 16777215).toString(16);
+      return '#' + Math.floor(Math.random() * 16777215).toString(16);
     };
 
     logo.addEventListener('click', function(e) {
-        toggleTheme();
+      toggleTheme();
     }, false);
 
     toggleTheme();
-};
-
-domReady(init);
+  }
+})();
